8f92cb4bba112de3f68650f1dd07d883f854e437
Bri Halterman <halterma@gmail.com>
2024-03-31 22:58:59 -0400

    Add description comments to challenge files

--- multiplication_table_generator.js
+++ multiplication_table_generator.js
@@ -1,3 +1,7 @@
+// 2. Print a table containing multiplication tables
+//    Can you print a table that contains all the answers to the multiplication tables from 1 through 10?
+//    Can you create an efficient solution that you could easily expand should you need the 12 times table?
+
 generateMultiplicationTable = function(n) {
   let multiplicationTable = []
   for (let i = 0; i <= n; i++) {
--- print_even_numbers.js
+++ print_even_numbers.js
@@ -1,3 +1,8 @@
+// 1. Print all even numbers from 0 – 10
+//    Try to make the solution to this problem as efficient as possible.
+//    Consider using loops that would allow you to type the fewest    characters/commands.
+//    Get creative and see how you could output them in a way that would work up to 10 or even up to 10,000 with little extra effort.
+
 printEvenNumbers = function(n) {
   let evenNumbers = [];
   for (let i = 0; i <= n; i++) {
